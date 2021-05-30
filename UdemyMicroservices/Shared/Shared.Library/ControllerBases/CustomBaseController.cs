5ce98521fff7d30812f44553b37f095eefa22246
Seyyid <seyyid364@gmail.com>
2021-05-30 16:39:56 +0300

    Create custom base controller for automatic return status code.

---
+++ UdemyMicroservices/Shared/Shared.Library/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Library.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Library.ControllerBases
+{
+    public class CustomBaseController: ControllerBase
+    {
+
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+
+    }
+}
