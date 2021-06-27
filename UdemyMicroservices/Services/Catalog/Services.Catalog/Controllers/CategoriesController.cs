51bb2f2661af59efeb779a76027c754aab2d10f5
Seyyid <seyyid364@gmail.com>
2021-06-27 09:29:55 +0300

    Added Categories Controller and edited category services create cetegory area.

---
+++ UdemyMicroservices/Services/Catalog/Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Catalog.Dtos;
+using Services.Catalog.Services;
+using Shared.Library.ControllerBases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Catalog.Controllers
+{
+    [Area("api")]
+    [Route("[area]/v{version:apiVersion}/Categories")]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "api")]
+    [ApiController]
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+
+            var response = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById(string Id)
+        {
+
+            var response = await _categoryService.GetByIdAsync(Id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var response = await _categoryService.CreateAsync(categoryDto);
+
+            return CreateActionResultInstance(response);
+        }
+    }
+}
