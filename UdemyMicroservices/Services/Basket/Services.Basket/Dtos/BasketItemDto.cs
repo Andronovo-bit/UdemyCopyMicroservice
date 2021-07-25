e3f50152c084056d89f8e73f18936fa17c19df91
Seyyid <seyyid364@gmail.com>
2021-07-25 16:40:41 +0300

    Added basket and basket item view models.

---
+++ UdemyMicroservices/Services/Basket/Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,12 @@
+namespace Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+    
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
