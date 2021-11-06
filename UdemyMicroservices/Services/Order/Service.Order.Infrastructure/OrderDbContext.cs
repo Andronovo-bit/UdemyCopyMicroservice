fe4e4d6725557fc9df42ffe79e71dbf8b571e971
Seyyid <seyyid364@gmail.com>
2021-11-06 21:46:24 +0300

    Created dbcontex for order with ef core.

---
+++ UdemyMicroservices/Services/Order/Service.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Order.Domain.OrderAggregate;
+
+ 
+
+namespace Service.Order.Infrastructure
+{
+    public class OrderDbContext: DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Services.Order.Domain.OrderAggregate.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Services.Order.Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<OrderItem>().ToTable("Orders", DEFAULT_SCHEMA);
+
+            modelBuilder.Entity<OrderItem>().Property(t => t.Price).HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Services.Order.Domain.OrderAggregate.Order>().OwnsOne(o => o.Address).WithOwner();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
