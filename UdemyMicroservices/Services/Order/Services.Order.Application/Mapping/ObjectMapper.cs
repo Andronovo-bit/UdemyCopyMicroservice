0063d9bca2643e94c25ccf25ca9bb10a645901ef
Seyyid <seyyid364@gmail.com>
2021-11-06 22:59:55 +0300

    Added autompper and created mapping class for dto to enttiy or reverse.

---
+++ UdemyMicroservices/Services/Order/Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Order.Application.Mapping
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>  // Ne zaman çağrılırsa o zaman burası çalışacak(initilize)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
