5abbe404dcaee607658d07fbd22602b4306ece62
Seyyid <seyyid364@gmail.com>
2021-11-06 22:04:06 +0300

    Created Datatable objects address, order and order items.

---
+++ UdemyMicroservices/Services/Order/Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Order.Application.Dtos
+{
+    public class OrderDto
+    {
+
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } //propery
+
+        public AddressDto Address { get; set; }
+
+        public string BuyerId { get; set; }
+
+        public List<OrderItemDto> OrderItems { get; set; }
+
+    }
+}
