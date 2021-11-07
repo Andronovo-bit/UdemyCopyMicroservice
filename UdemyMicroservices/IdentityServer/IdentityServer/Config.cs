3762ab42faba5e1670272295c4c34d451f1afb9e
Seyyid <seyyid364@gmail.com>
2021-11-07 19:48:03 +0300

    Added order microservice to identity server config and order api startup added identity server configs.

--- UdemyMicroservices/IdentityServer/IdentityServer/Config.cs
+++ UdemyMicroservices/IdentityServer/IdentityServer/Config.cs
@@ -19,6 +19,7 @@ namespace IdentityServer
                 new ApiResource("resource_photo_stock"){Scopes = {"photo_stock_fullpermission"}},
                 new ApiResource("resource_basket"){Scopes = {"basket_fullpermission"}},
                 new ApiResource("resource_discount"){Scopes = {"discount_fullpermission"}},
+                new ApiResource("resource_order"){Scopes = {"order_fullpermission"}},
                 new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -45,6 +46,7 @@ namespace IdentityServer
                 new ApiScope("photo_stock_fullpermission","Full Access for Photo Stock API"),
                 new ApiScope("basket_fullpermission","Full Access for Basket API"),
                 new ApiScope("discount_fullpermission","Full Access for Discount API"),
+                new ApiScope("order_fullpermission","Full Access for Order API"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -82,6 +84,7 @@ namespace IdentityServer
                     {
                         "basket_fullpermission",
                         "discount_fullpermission",
+                        "order_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.Address,
                         IdentityServerConstants.StandardScopes.Phone,
--- UdemyMicroservices/Services/Order/Services.Order.API/Startup.cs
+++ UdemyMicroservices/Services/Order/Services.Order.API/Startup.cs
@@ -1,13 +1,21 @@
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Service.Order.Infrastructure;
+using Shared.Library.Services;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,7 +34,34 @@ namespace Services.Order.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            var requereAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_order";
+                options.RequireHttpsMetadata = false;
+            }); 
+
+            services.AddDbContext<OrderDbContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), configure =>
+                {
+                    configure.MigrationsAssembly("Service.Order.Infrastructure");
+                });
+            });
+
+            services.AddHttpContextAccessor();
+
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+            services.AddMediatR(typeof(Application.Handlers.Command.CreateOrderCommandHandler).Assembly);
+
+            services.AddControllers(opt => {
+                opt.Filters.Add(new AuthorizeFilter(requereAuthorizePolicy));
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Services.Order.API", Version = "v1" });
@@ -45,6 +80,7 @@ namespace Services.Order.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
