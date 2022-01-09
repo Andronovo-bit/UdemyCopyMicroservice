fa23bba75a09d17b9c9a180d6cd3fa5e7127d7dc
Seyyid <seyyid364@gmail.com>
2022-01-09 13:37:00 +0300

    Create Identity service, signin and errordto model.

---
+++ UdemyMicroservices/Frontends/Web/Services/Abstract/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿using IdentityModel.Client;
+using Shared.Library.Dtos;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Services.Abstract
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+        Task RevokeRefreshToken();
+    }
+}
